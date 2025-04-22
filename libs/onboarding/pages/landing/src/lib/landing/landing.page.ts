fff800f64487d4b778c6e4e5a5a27205c666e8bc
StevoPorridge <stevoporridge@gmail.com>
2025-04-22 11:40:34 +0100

    feat: add logo and colour to landing page for onboarding

--- libs/onboarding/pages/landing/src/lib/landing/landing.page.ts
+++ libs/onboarding/pages/landing/src/lib/landing/landing.page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonButton, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'onboarding-landing',
-  imports: [CommonModule],
+  imports: [CommonModule, IonContent, IonButton],
   templateUrl: './landing.page.html',
   styleUrl: './landing.page.scss',
 })
